95048984ef4e5552de32dc7d921d9d9a14e84bd1
Juliana Valverde <valverde.juliana@gmail.com>
2021-10-25 13:18:12 +0200

    Added time
    Added the functions for the time and updated the design to reflect it

--- src/app.js
+++ src/app.js
@@ -1,16 +1,42 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#currentTemperature");
-  let cityEmelement = document.querySelector("#currentCity");
-  let descriptionEmelement = document.querySelector("#currentDescription");
+  let cityElement = document.querySelector("#currentCity");
+  let descriptionElement = document.querySelector("#currentDescription");
   let feelElement = document.querySelector("#currentFeel");
   let humidityElement = document.querySelector("#currentHumidity");
   let windElement = document.querySelector("#currentWind");
+  let dateElement = document.querySelector("#currentDate");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityEmelement.innerHTML = response.data.name;
-  descriptionEmelement.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   feelElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
