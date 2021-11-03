304f43d3618555a2c0665c4bd4f8827d51cd0f0f
Juliana Valverde <valverde.juliana@gmail.com>
2021-11-03 13:44:54 +0100

    API call for the forecast

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ function formatDate(timestamp) {
 }
 
 // display forecast days
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -55,6 +56,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  // console.log(coordinates);
+  let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+
 // handles the display of temperature, city, humidity and other parameters from the API
 function displayTemperature(response) {
   // console.log(response.data);
@@ -81,13 +90,14 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 // search engine function
 function search(city) {
   // API call
   let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
-  // let city = "Amsterdam";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
@@ -131,4 +141,3 @@ let celciusLink = document.querySelector("#toCelcius");
 celciusLink.addEventListener("click", showCelciusTemperature);
 
 search("Amsterdam");
-displayForecast();
